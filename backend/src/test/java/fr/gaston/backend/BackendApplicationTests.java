d43c75fb8a87179f9932d61bb884bb00b8464dd7
Gaston Pallas <pallasgaston@gmail.com>
2024-10-26 13:01:50 +0200

    test docker profile

--- backend/src/test/java/fr/gaston/backend/BackendApplicationTests.java
+++ backend/src/test/java/fr/gaston/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package fr.gaston.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("docker")
 class BackendApplicationTests {
 
     @Test
